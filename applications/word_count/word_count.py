9eec60c8337a763fae90dac92cbbf93c241a1019
Milo Rastgoo <msrastgoo@gmail.com>
2020-06-04 09:45:36 -0400

    word count completed

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,28 @@
 def word_count(s):
     # Your code here
+    punc = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'
+    punc_list = punc.split()
+    punc_dict = {i:'' for i in punc}
+    s = s.lower()
+    # print(s)
+    string_list = s.split()
+    new_string_list = []
+    for word in string_list:
+        new_word = ""
+        for c in word:
+            if c not in punc_dict:
+                new_word += c
+        if len(new_word) > 0:
+            new_string_list.append(new_word)
+    print(new_string_list)
+    string_dict = {}
+    for word in new_string_list:
+        if word in string_dict:
+            string_dict[word] += 1
+        else:
+            string_dict[word] = 1
+    print(string_dict)
+    return string_dict
 
 
 
